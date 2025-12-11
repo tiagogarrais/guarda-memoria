5f56a2c0b2fd219d92adf3452674119f28c69eaf
Tiago Arrais <tiagoarrais1981@hotmail.com>
2025-12-11 08:02:53 -0300

    Atualizando Next.js para a versão 15

--- app/components/LocationSelector.js
+++ app/components/LocationSelector.js
@@ -101,7 +101,7 @@ export default function LocationSelector({
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
       <h2 className="text-2xl font-bold mb-4 text-center">
-        {currentLocation ? "Trocar Localização" : "Selecione sua localização"}
+        {currentLocation ? "Escolher localização" : "Escolher localização"}
       </h2>
 
       {currentLocation && (
